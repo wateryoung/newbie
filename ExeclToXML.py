637e943379b7263b159bdc0ae30316f71546fb35
wateryoung <47383184+wateryoung@users.noreply.github.com>
2022-02-07 13:13:28 +0900

    Update ExeclToXML.py

--- ExeclToXML.py
+++ ExeclToXML.py
@@ -1,9 +1,15 @@
 # Install the openpyxl library
 from openpyxl import load_workbook
-from yattag import Doc, indent
+from yattag import Doc, indent, EACH_LINE
+
+# for chdir()
+import os
+
+# 작업 디렉토리로 이동한다.
+os.chdir("C:/workpy/ExcelToXML")
 
 # Loading our Excel file
-wb = load_workbook("G:\workpy\data\접속표준서(정보분배-UDP_TCP실시간)_v1.02.xlsx")
+wb = load_workbook("./SRC/접속표준서(정보분배-UDP_TCP실시간)_v1.02.xlsx")
 
 # Getting an object of active sheet 4th
 ws = wb.worksheets[3]
@@ -18,36 +24,39 @@ xml_schema = '<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"></xs:schema
 doc.asis(xml_header)
 doc.asis(xml_schema)
   
-with tag('InterfaceDefinition'):
+with tag('InterfaceDefDoc'):    #인터페이스정의서
     for row in ws.iter_rows(min_row=3, max_row=3977, min_col=1, max_col=13):
-        row = [cell.value for cell in row]
+        col = [cell.value for cell in row]
+        print(col)
         with tag("Row"):
             with tag("Interface_ID"):
-                text(row[0])
+                text(col[0])
             with tag("Inferface_Name"):
-                text(row[1])
+                text(col[1])
             with tag("Sequence"):
-                text(row[3])
+                text(col[3])
             with tag("ItemName"):
-                text(row[4])
+                text(col[4])
             with tag("Depth"):
-                text(row[6])
+                text(col[6])
             with tag("DataType"):
-                text(row[7])
+                text(col[7])
             with tag("Length"):
-                text(row[8])  
+                text(col[8])  
             with tag("AccLength"):
-                text(row[9])
-#            with tag("Definition"):
-#                text(row[10])
-#            with tag("Remarks"):
-#                text(row[12])
+                text(col[9])
+            with tag("Definition"):
+                text(col[10])
+            with tag("Remarks"):
+                if col[12]:
+                    text(col[12])
 
 result = indent(
     doc.getvalue(),
     indentation='   ',
-    indent_text=True
+    newline = '\n',
+    indent_text=False
 )
   
-with open("output.xml", "w") as f:
+with open("./XML/krx_spec_inf_splt.xml", "w", encoding='UTF-8') as f:
     f.write(result)
